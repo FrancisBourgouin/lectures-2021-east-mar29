4f5a2d7dc45060f542225890961b0792cb73ed27
Francis Bourgouin <netbeuix@gmail.com>
2021-03-30 11:56:51 -0400

    added w1d2 lecture content

---
+++ w1d2/example.js
@@ -0,0 +1,22 @@
+// Function declaration / definition
+
+function iAmAFunction() {
+  console.log('I am a super cool function I guess')
+}
+
+iAmAFunction()
+
+
+function iAmAFunction() {
+  console.log('Mwahahaha I wrote something else')
+}
+
+// Function expression
+
+const iAmAnotherFunction = function () {
+  return 'I like potatoes'
+}
+
+
+
+
---
+++ w1d2/sum.js
@@ -0,0 +1,79 @@
+// Take all the values from the arguments in the terminal, add them together and output the sum
+// Positive & negative values needs to be accounted for, and everything else should be ignored
+
+// Making a plan, assess what you have
+
+// Expected input & output
+// $ node sum.js 1 2 3 4 5 -1 potato
+// should return 14
+
+// What do we have
+// Values from the command line (we get an array with process.argv)
+
+// What do we want
+// Sum of all valid values
+// (pos / neg) good, (everything else) bad
+
+
+// How can we achieve that
+// query process.argv, and clean it by using the slice method (cleaning is optionnal)
+// Declare an accumulator
+// iterate over the elements of the process.argv array
+// for every element in the array
+// convert, then check if string or number
+// if is a number
+// update the value of the accumulator with the new number
+// return the accumulator
+
+
+
+
+// Create a function called sum
+// Get the arguments from the command line
+// Call the function
+// Consider the constraints
+// Create some pseudo code
+// process.argv store that somewhere
+
+
+// ACTUAL CODING TIME !
+
+
+// query process.argv, and clean it by using the slice method (cleaning is optionnal)
+const commandLineArguments = process.argv.slice(2)
+console.log(commandLineArguments)
+
+const sumOfAListOfElements = function (listOfElements) {
+  // Declare an accumulator
+  let accumulator = 0
+  // iterate over the elements of the process.argv array
+  for (let i = 0; i < listOfElements.length; i++) {
+    // for every element in the array
+    const currentElement = listOfElements[i]
+    // convert, 
+    const parsedElement = Number(currentElement)
+    // then check if string or number
+    if (!Number.isNaN(parsedElement)) {
+      // if is a number
+      // update the value of the accumulator with the new number
+      accumulator += parsedElement
+    }
+  }
+  // return the accumulator
+  return accumulator
+}
+
+const sumOfAListOfElements = function (listOfElements) {
+  let accumulator = 0
+  for (let i = 0; i < listOfElements.length; i++) {
+    const currentElement = listOfElements[i]
+    const parsedElement = Number(currentElement)
+    if (!Number.isNaN(parsedElement)) {
+      accumulator += parsedElement
+    }
+  }
+  return accumulator
+}
+
+const output = sumOfAListOfElements(commandLineArguments)
+console.log(output)
